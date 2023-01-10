38f3c01708b685124fd67ada13e703efbc7af6b3
Victor Almada <almadavic@live.com>
2023-01-10 18:20:57 -0300

    Project Created

---
+++ src/main/java/com/almadavic/securitystandard/SecurityStandardApplication.java
@@ -0,0 +1,13 @@
+package com.almadavic.securitystandard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityStandardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almadavic/securitystandard/SecurityStandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almadavic.securitystandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityStandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
